a395439dfa56e4254e75caec68f09f8501a76c7c
ptlx <ptlx@outlook.jp>
2022-06-11 00:52:11 +0900

    簡単なスタイル修正，style.cssの移動

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,6 @@
-import React  from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./style.css";
 import { Home } from "../home/home";
-
 
 const App: React.FC = () => {
     return (
@@ -12,7 +10,6 @@ const App: React.FC = () => {
                     <Route index element={<Home/>}/>
                 </Routes>
             </Router>
-
         </>
     );
 }
